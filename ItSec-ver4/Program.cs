441107c5f53305a1f73e2e122e300411ba699d27
mafj0362 <117722751+mafj0362@users.noreply.github.com>
2023-04-10 17:42:08 +0200

    Add files via upload

---
+++ ItSec-ver4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ItSec_ver4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello"); 
+        }
+        public static void Welcome()
+        {
+
+        }
+    }
+}
